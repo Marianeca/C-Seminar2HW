d72f814274b52addabcd46b9adc9d5a1f4c87c85
Mar <marianeca.of@gmail.com>
2022-12-04 20:01:35 +0300

    Test10 done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿// Напишиет программу, которая принимает на входе трехзначное число и на выходе показывает вторую цифру этого числа.
 
-int a;
 
 Console.WriteLine($"Введите трехзначное число: ");
-string number(Console.ReadLine()!, out a);
+string number = Console.ReadLine()!;
 
-Console.Write($"Цифра = {number[1]}");
+Console.Write($"Вторая цифра = {number[1]}\n");
